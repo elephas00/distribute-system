8320335c88ccb765ee3beca9de46d75b9cef2da6
yongjiang <yongjianglong00@gmail.com>
2023-09-18 22:26:28 +0800

    first commit

--- src/main/mrworker.go
+++ src/main/mrworker.go
@@ -10,11 +10,14 @@ package main
 // Please do not change this file.
 //
 
-import "6.5840/mr"
-import "plugin"
-import "os"
-import "fmt"
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+	"plugin"
+
+	"6.5840/mr"
+)
 
 func main() {
 	if len(os.Args) != 2 {
@@ -23,7 +26,7 @@ func main() {
 	}
 
 	mapf, reducef := loadPlugin(os.Args[1])
-
+	println("workder main")
 	mr.Worker(mapf, reducef)
 }
 
